2a5be8c49ab7276d8af1a65bd96e2c55eef4de46
Philip John <95682231+Philip-Nwabuwa@users.noreply.github.com>
2022-10-07 10:27:49 +0000

    installing firebase

---
+++ src/Firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsRgXGxsCA5m2yS6tEb3YZXdvEuyOYGZA",
+  authDomain: "calculator-alt-school.firebaseapp.com",
+  projectId: "calculator-alt-school",
+  storageBucket: "calculator-alt-school.appspot.com",
+  messagingSenderId: "476845365774",
+  appId: "1:476845365774:web:e760e57382bdfaab477a5f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
